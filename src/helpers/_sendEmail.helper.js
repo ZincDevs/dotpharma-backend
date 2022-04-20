ec99db57c52f232435332160cdbb5dd91f57382e
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-21 00:16:38 +0200

    fix bug

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -12,7 +12,7 @@ export default async (emailTo, subject, template) => {
     const transporter = mailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
-      secure: true,
+      secure: false,
       tls: {
         ciphers: 'SSLv3'
       },
