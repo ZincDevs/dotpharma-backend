4428bde338210f14cd4e2c0ffa382c4199022fc6
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-21 00:48:37 +0200

    fix bug

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -5,14 +5,12 @@ import mailer from 'nodemailer';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 export default async (emailTo, subject, template) => {
-  console.log(`PID: ${process.pid} === SENDING EMAIL ===`);
-  console.log(process.env.EMAIL_SENDER, process.env.PASSWORD_EMAIL_SENDER);
   try {
     const transporter = mailer.createTransport({
       host: 'smtp.gmail.com',
-      service: 'gmail',
+      port: 587,
+      secure: false,
       tls: {
         ciphers: 'SSLv3'
       },
@@ -23,42 +21,18 @@ export default async (emailTo, subject, template) => {
       },
     });
 
-    await new Promise((resolve, reject) => {
-      transporter.verify((error, success) => {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log('Server is ready to take our messages');
-          resolve(success);
-        }
-      });
-    });
-
-    const mailData = {
-      from: {
-        name: 'DotPharma',
-        address: process.env.EMAIL_SENDER
-      },
-      replyTo: process.env.EMAIL_SENDER,
+    const res = await transporter.sendMail({
+      from: `DotPharma<${process.env.EMAIL_SENDER}>`,
       to: emailTo,
       subject,
       text: 'Hello',
       html: template
-    };
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-          console.error(err);
-          console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
-          reject(err);
-        } else {
-          console.log(info);
-          console.log(`PID: ${process.pid} === EMAIL SENT ===`);
-          resolve(info);
-        }
-      });
     });
+
+    res.accepted ? console.log(`PID: ${process.pid} === EMAIL SENT ===`)
+      : console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
+
+    return res;
   } catch (error) {
     return error;
   }
