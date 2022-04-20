9ea8d0b0e2d83e048dbb8dbbf61cba13ecdfb35d
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-21 01:11:48 +0200

    fix bug

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -11,9 +11,6 @@ export default async (emailTo, subject, template) => {
       host: 'smtp.gmail.com',
       port: 587,
       secure: false,
-      tls: {
-        ciphers: 'SSLv3'
-      },
       from: process.env.EMAIL_SENDER,
       auth: {
         user: process.env.EMAIL_SENDER,
@@ -21,18 +18,44 @@ export default async (emailTo, subject, template) => {
       },
     });
 
-    const res = await transporter.sendMail({
-      from: `DotPharma<${process.env.EMAIL_SENDER}>`,
+    await new Promise((resolve, reject) => {
+      transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('=== SERVER IS READY TO SEND EMAIL ===');
+          resolve(success);
+        }
+      });
+    });
+
+    const mailData = {
+      from: {
+        name: 'DotPharma',
+        address: process.env.EMAIL_SENDER,
+      },
+      replyTo: process.env.EMAIL_SENDER,
       to: emailTo,
       subject,
       text: 'Hello',
       html: template
+    };
+
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(`PID: ${process.pid} === EMAIL SENT ===`);
+          console.log(info);
+          resolve(info);
+        }
+      });
     });
-
-    res.accepted ? console.log(`PID: ${process.pid} === EMAIL SENT ===`)
-      : console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
-
-    return res;
+    return mailData;
   } catch (error) {
     return error;
   }
