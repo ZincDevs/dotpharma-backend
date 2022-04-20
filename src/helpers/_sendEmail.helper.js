bc39e3904ad775395033307363f36b7c19fa0326
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-20 22:53:28 +0200

    fix send mail

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -11,7 +11,7 @@ export default async (emailTo, subject, template) => {
     const transporter = mailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
-      secure: false,
+      secure: f,
       tls: {
         ciphers: 'SSLv3'
       },
@@ -22,18 +22,42 @@ export default async (emailTo, subject, template) => {
       },
     });
 
-    const res = await transporter.sendMail({
-      from: `DotPharma<${process.env.EMAIL_SENDER}>`,
+    await new Promise((resolve, reject) => {
+      transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Server is ready to take our messages');
+          resolve(success);
+        }
+      });
+    });
+
+    const mailData = {
+      from: {
+        name: 'DotPharma',
+        address: process.env.EMAIL_SENDER
+      },
+      replyTo: process.env.EMAIL_SENDER,
       to: emailTo,
       subject,
       text: 'Hello',
       html: template
+    };
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
+          reject(err);
+        } else {
+          console.log(info);
+          console.log(`PID: ${process.pid} === EMAIL SENT ===`);
+          resolve(info);
+        }
+      });
     });
-
-    res.accepted ? console.log(`PID: ${process.pid} === EMAIL SENT ===`)
-      : console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
-
-    return res;
   } catch (error) {
     return error;
   }
