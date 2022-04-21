b16288808748f463685f6fb23a63c9b10eaf8b5d
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-21 02:49:00 +0200

    fix bug

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 import 'regenerator-runtime';
@@ -9,6 +10,31 @@ dotenv.config();
 export default async (emailTo, subject, template) => {
   console.log(`PID: ${process.pid} === SENDING EMAIL ===`);
   try {
+    const mailer = nodemailer.createTransport(
+      transport({
+        auth: {
+          api_key: process.env.SENDGRID_API_KEY,
+        },
+      })
+    );
+
+    const email_option = {
+      to: emailTo,
+      replyTo: process.env.EMAIL_SENDER,
+      from: `DotPharma <${process.env.EMAIL_SENDER}>`,
+      subject,
+      text: subject,
+      html: template,
+    };
+
+    mailer.sendMail(email_option, (err, info) => {
+      if (err) {
+        console.log(err, `PID: ${process.pid} === EMAIL NOT SENT ===`);
+      } else {
+        console.log(info, `PID: ${process.pid} === EMAIL SENT ===`);
+      }
+    });
+
     // const transporter = mailer.createTransport({
     //   host: 'smtp.gmail.com',
     //   port: 587,
@@ -20,13 +46,13 @@ export default async (emailTo, subject, template) => {
     //   },
     // });
 
-    const mailer = nodemailer.createTransport(
-      transport({
-        auth: {
-          api_key: process.env.SENDGRID_API_KEY,
-        },
-      })
-    );
+    // const mailer = nodemailer.createTransport(
+    //   transport({
+    //     auth: {
+    //       api_key: process.env.SENDGRID_API_KEY,
+    //     },
+    //   })
+    // );
 
     // await new Promise((resolve, reject) => {
     //   transporter.verify((error, success) => {
@@ -40,32 +66,32 @@ export default async (emailTo, subject, template) => {
     //   });
     // });
 
-    const mailData = {
-      from: {
-        name: 'DotPharma',
-        address: process.env.EMAIL_SENDER,
-      },
-      replyTo: process.env.EMAIL_SENDER,
-      to: emailTo,
-      subject,
-      text: 'Hello',
-      html: template
-    };
+    // const mailData = {
+    //   from: {
+    //     name: 'DotPharma',
+    //     address: process.env.EMAIL_SENDER,
+    //   },
+    //   replyTo: process.env.EMAIL_SENDER,
+    //   to: emailTo,
+    //   subject,
+    //   text: 'Hello',
+    //   html: template
+    // };
 
-    await new Promise((resolve, reject) => {
-      mailer.sendMail(mailData, (err, info) => {
-        if (err) {
-          console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(`PID: ${process.pid} === EMAIL SENT ===`);
-          console.log(info);
-          resolve(info);
-        }
-      });
-    });
-    return mailData;
+    // await new Promise((resolve, reject) => {
+    //   mailer.sendMail(mailData, (err, info) => {
+    //     if (err) {
+    //       console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
+    //       console.error(err);
+    //       reject(err);
+    //     } else {
+    //       console.log(`PID: ${process.pid} === EMAIL SENT ===`);
+    //       console.log(info);
+    //       resolve(info);
+    //     }
+    //   });
+    // });
+    return email_option;
   } catch (error) {
     return error;
   }
