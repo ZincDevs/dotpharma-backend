074e57ed1fcd55f607d5a542f9d18712fb247b8d
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-22 02:39:27 +0200

    fix error

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -2,34 +2,51 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 import 'regenerator-runtime';
-// import nodemailer from 'nodemailer';
+import mailer from 'nodemailer';
 import dotenv from 'dotenv';
-// import transport from 'nodemailer-sendgrid-transport';
-import sgMail from '@sendgrid/mail';
 
 dotenv.config();
-export default async (emailTo, subject, template, finish) => {
+
+export default async (emailTo, subject, template) => {
   console.log(`PID: ${process.pid} === SENDING EMAIL ===`);
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const email_option = {
-    to: emailTo,
-    replyTo: process.env.EMAIL_SENDER,
-    from: `DotPharma <${process.env.EMAIL_SENDER}>`,
-    subject,
-    text: subject,
-    html: template,
-  };
-  sgMail
-    .send(email_option)
-    .then((info) => {
-      console.log(info, `PID: ${process.pid} === EMAIL SENT ===`);
-      finish(email_option);
-      // resolve(info);
-    })
-    .catch((error) => {
-      console.log(error, `PID: ${process.pid} === EMAIL NOT SENT ===`);
-      finish(email_option);
-      // reject(error);
+  try {
+    const transporter = mailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        type: 'OAuth2',
+        clientId: '327280420092-o196gt7q0hcrdpj51c9vqhqb3tgn85pk.apps.googleusercontent.com',
+        clientSecret: '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'
+      }
     });
-  return email_option;
+
+    const mailOptions = {
+      from: `DotPharma<${process.env.EMAIL_SENDER}>`,
+      to: emailTo,
+      subject,
+      text: 'Hello',
+      html: template,
+      auth: {
+        user: process.env.EMAIL_SENDER,
+        refreshToken: '1//04rAqukGDI78CCgYIARAAGAQSNwF-L9Ir99quhuUpBZpTbe85JOxm3b4jVmj5J7Ym5Napn4PZfVuBGsZGuIVbLxOEp5GTE8xXCFs',
+        access_token: "ya29.A0ARrdaM9PYiy1Dk_PJKzSa4-J6DLoM_lOVC0S9ETOpZdRkT3lUVlZ6eKUGZkOvcZ-hvCy4UtjItBMk-3vChcCky1uV3_V3aCEZb32WTo99RXWhfF6VjaNRfwpaNkGDISVb6znYvhhjsNsqjwG-YaLT75HRAYi',",
+        accessToken: 'ya29.A0ARrdaM9PYiy1Dk_PJKzSa4-J6DLoM_lOVC0S9ETOpZdRkT3lUVlZ6eKUGZkOvcZ-hvCy4UtjItBMk-3vChcCky1uV3_V3aCEZb32WTo99RXWhfF6VjaNRfwpaNkGDISVb6znYvhhjsNsqjwG-YaLT75HRAYi',
+        expires: 1494388182480
+      }
+    };
+
+    transporter.sendMail(mailOptions, (error, response) => {
+      if (error) {
+        console.log(error);
+        console.log(`PID: ${process.pid} === EMAIL NOT SENT ===`);
+      } else {
+        console.log(response);
+        console.log(`PID: ${process.pid} === EMAIL SENT ===`);
+      }
+      transporter.close();
+    });
+
+    return mailOptions;
+  } catch (error) {
+    return error;
+  }
 };
