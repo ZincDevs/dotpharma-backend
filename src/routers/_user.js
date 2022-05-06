163566af510618b2ef0024ec599fede366482790
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 20:57:38 +0200

    update _user.js

--- src/routers/_user.js
+++ src/routers/_user.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 import 'regenerator-runtime';
 import express from 'express';
 import User from '../controllers/UserController';
@@ -6,24 +8,34 @@ import Validator from '../middleware/_validator';
 import UserMiddle from '../middleware/user';
 import Paginate from '../middleware/Paginate';
 
+const {
+  verifyAccessToken,
+  verifyUserVerificationToken,
+  verifyUPasswordResetToken
+} = Auth;
+
 const router = express.Router();
-// Here user is added by admin
-router.post('/createuser', Validator('createuser'), Auth.verifyToken, UserMiddle.checkUserExists, User.createUser);
-// Here register them selves
-router.post('/signup', Validator('signup'), UserMiddle.checkUserExists, User.signup);
-router.put('/updateuser/:uid', Validator('updateuser'), Auth.verifyToken, User.update);
-router.get('/allusers', Auth.verifyToken, Paginate, User.findAll);
-router.delete('/deleteuser/:uid', Auth.verifyToken, User.destroy);
-
-router.put('/request-password-reset', Validator('resetpass'), Auth.verifyToken, User.requestPasswordReset);
-
-router.put('/activateuser/:token', Auth.verifyToken2, User.validateUserAccount);
-router.post('/resendverification', UserMiddle.checkUserExists2, User.resendVerification);
 
 // Authentication and authorization
 router.post('/login', UserMiddle.checkUserExists2, Auth.checkCredentials, User.login);
 router.get('/logout', User.logout);
 router.get('/refresh-token', User.refreshToken);
 router.post('/admin/login', UserMiddle.checkUserExists2, UserMiddle.checkISAdmin, User.login);
+
+// User verirication and confirmation
+router.put('/activateuser/:token', verifyUserVerificationToken, User.verifyUserAccount);
+router.post('/resendverification', UserMiddle.checkUserExists2, User.resendVerification);
+
+// Users && signup
+router.post('/createuser', Validator('createuser'), verifyAccessToken, UserMiddle.checkUserExists, User.createUser);
+router.post('/signup', Validator('signup'), UserMiddle.checkUserExists, User.signup);
+router.put('/updateuser/:uid', Validator('updateuser'), verifyAccessToken, User.update);
+router.get('/allusers', verifyAccessToken, Paginate, User.findAll);
+router.delete('/deleteuser/:uid', verifyAccessToken, User.destroy);
+
+// Password reset
+router.post('/request-password-reset', UserMiddle.checkUserExists2, User.requestPasswordReset);
+router.put('/apply-password-reset', Validator('resetpass'), verifyUPasswordResetToken, User.confirmPasswordReset);
+router.post('/resend-password-reset', UserMiddle.checkUserExists2, User.resendPasswordReset);
 
 export default router;
