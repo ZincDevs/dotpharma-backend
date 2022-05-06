b1a14888e89798874720dcd37a7eabdafdd7c638
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 20:58:12 +0200

    update src/templates/index.js

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import 'regenerator-runtime';
 import verificationTemplate from './_accountVerification.template';
+import passWordResetTemplate from './_passwordReset.template';
 
 export {
   verificationTemplate,
+  passWordResetTemplate,
 };
