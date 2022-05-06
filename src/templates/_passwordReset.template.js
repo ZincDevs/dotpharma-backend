08166971636d7919ab464feaf8e9f0018a4ba2f0
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 20:57:46 +0200

    feat: create _passwordReset.template.js

---
+++ src/templates/_passwordReset.template.js
@@ -0,0 +1,249 @@
+import 'regenerator-runtime';
+import { constants } from '../config';
+
+const { logoUrl } = constants;
+export default (link) => `
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Email Confirmation</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap" rel="stylesheet" />
+  <style type="text/css">
+    /**
+   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+   */
+    @media screen {
+      @font-face {
+        font-family: 'Source Sans Pro';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+      }
+
+      @font-face {
+        font-family: 'Source Sans Pro';
+        font-style: normal;
+        font-weight: 700;
+        src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+      }
+    }
+
+    /**
+   * Avoid browser level font resizing.
+   * 1. Windows Mobile
+   * 2. iOS / OSX
+   */
+    body,
+    table,
+    td,
+    a {
+      -ms-text-size-adjust: 100%;
+      /* 1 */
+      -webkit-text-size-adjust: 100%;
+      /* 2 */
+    }
+
+    /**
+   * Remove extra space added to tables and cells in Outlook.
+   */
+    table,
+    td {
+      mso-table-rspace: 0pt;
+      mso-table-lspace: 0pt;
+    }
+
+    /**
+   * Better fluid images in Internet Explorer.
+   */
+    img {
+      -ms-interpolation-mode: bicubic;
+    }
+
+    /**
+   * Remove blue links for iOS devices.
+   */
+    a[x-apple-data-detectors] {
+      font-family: inherit !important;
+      font-size: inherit !important;
+      font-weight: inherit !important;
+      line-height: inherit !important;
+      color: inherit !important;
+      text-decoration: none !important;
+    }
+
+    /**
+   * Fix centering issues in Android 4.4.
+   */
+    div[style*="margin: 16px 0;"] {
+      margin: 0 !important;
+    }
+
+    body {
+      width: 100% !important;
+      height: 100% !important;
+      padding: 0 !important;
+      margin: 0 !important;
+    }
+
+    /**
+   * Collapse table borders to avoid space between cells.
+   */
+    table {
+      border-collapse: collapse !important;
+    }
+
+    a {
+      color: #2B8DD0;
+      font-family: 'Dosis', sans-serif;
+    }
+
+    img {
+      height: auto;
+      line-height: 100%;
+      text-decoration: none;
+      border: 0;
+      outline: none;
+    }
+
+    h3 {
+      color: #2B8DD0;
+      text-align: center;
+      text-transform: uppercase;
+      font-weight: 300;
+      font-family: 'Dosis', sans-serif;
+      font-size: 1em;
+    }
+
+    strong {
+      margin-left: 4px;
+      font-weight: 800;
+      color: #48AC62;
+    }
+
+    strong::before,
+    strong::after {
+      content: ".";
+      font-family: dosis !important;
+    }
+
+    p {
+      font-family: dosis;
+      color: #909090;
+    }
+  </style>
+
+</head>
+
+<body style="background-color: #e9ecef;">
+  <!-- <div class="preheader"
+    style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+    Welcome to DotPharma. Here is your account verification link.
+  </div> -->
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+      <td align="center" bgcolor="#e9ecef">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="center" valign="top" style="padding: 36px 24px;">
+              <img src="${logoUrl}"
+                alt="Logo" border="0" width="48"
+                style="display: block; width: 48px; max-width: 48px; min-width: 48px;" />
+                <h3 align="center" style="align-self: center;">
+                  DOT
+                  <strong>PHARMA</strong>
+                </h3>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" bgcolor="#e9ecef">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #2B8DD0;">
+              <h1
+                style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px; color: #797979;">
+                Confirm Password Reset</h1>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" bgcolor="#e9ecef">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <p style="margin: 0;">You have requested to reset your password. Please tap the button below to confirm
+                your request. If you didn't request this action, you can simply ignore this message.</p>
+            </td>
+          </tr>
+          <tr>
+            <td align="left" bgcolor="#ffffff">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td align="center" bgcolor="#ffffff" style="padding: 12px;">
+                    <table border="0" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
+                          <a href="${link}" target="_blank"
+                            style="display: inline-block; padding: 16px 36px; font-family: 'Dosis', sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px; background-color: #2B8DD0; border-radius: 0;">
+                            Reset Password
+                          </a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <p style="margin: 0;">If that doesn't work, copy and paste the following link in your
+                browser:</p>
+              <p style="margin: 0;"><a href="${link}" style="text-decoration: none; color:#2B8DD0;"
+                  target="_blank">${link}</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #2B8DD0">
+              <p style="margin: 0;">Cheers,<br> DotPharma</p>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+
+    <tr>
+      <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+          <tr>
+            <td align="center" bgcolor="#e9ecef"
+              style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;">You received this email because we received a request to reset your password. If you
+                didn't request this action, you can safely
+                delete this email.</p>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
+`;
