5e9d77623bf09964913bbd036e237c0b47a7f677
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 20:57:30 +0200

    update src/services/index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import 'regenerator-runtime';
-import { sendVerificationService as sendVerification } from './_sendMail.service';
+import {
+  sendVerificationService as sendVerification,
+  sendPasswordResetConfirmationService as sendPasswordResetConfirmation
+} from './_sendMail.service';
 
 export {
   sendVerification,
+  sendPasswordResetConfirmation
 };
