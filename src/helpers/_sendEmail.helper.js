2d0ce7684a693a51d15c4fe7a9f1a81f96959168
benshidanny11 <benshidanny11@gmail.com>
2023-02-13 19:20:18 +0200

    Resolve email problems

--- src/helpers/_sendEmail.helper.js
+++ src/helpers/_sendEmail.helper.js
@@ -8,59 +8,38 @@
 // const dotenv = require('dotenv');
 import 'regenerator-runtime';
 import mailer from 'nodemailer';
+import { MailtrapClient } from 'mailtrap';
 import dotenv from 'dotenv';
 import { google } from 'googleapis';
 
 dotenv.config();
 const {
   EMAIL_SENDER,
+  OAUTH2_USER,
   OAUTH2_CLIENT_ID,
   OAUTH2_CLIENT_SECRET,
   OAUTH2_REDIRECT_URI,
   OAUTH2_REFRESH_TOKEN,
   PASSWORD_EMAIL_SENDER,
   MAIL_SENDER_HOST,
-  MAIL_SENDER_PORT
+  MAIL_SENDER_PORT,
+  MAIL_TRAP_TOKEN,
+  MAIL_TRAP_ENDPOINT
 } = process.env;
-// const oAuth2Client = new google.auth.OAuth2(OAUTH2_CLIENT_ID, OAUTH2_CLIENT_SECRET, OAUTH2_REDIRECT_URI);
-// oAuth2Client.setCredentials({ refresh_token: OAUTH2_REFRESH_TOKEN });
+
+const client = new MailtrapClient({ endpoint: MAIL_TRAP_ENDPOINT, token: MAIL_TRAP_TOKEN });
 
 const sentMail = async (emailTo, subject, template) => {
   console.log(`PID: ${process.pid} === SENDING EMAIL ===`);
-  try {
-    const transporter = mailer.createTransport({
-      name: 'dotpharma.rw',
-      host: MAIL_SENDER_HOST,
-      port: MAIL_SENDER_PORT,
-      secure: false,
-      auth: {
-        user: EMAIL_SENDER,
-        pass: PASSWORD_EMAIL_SENDER,
-      },
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false
-      }
-    });
-
-    const mailOptions = {
-      from: `DotPharma <${process.env.EMAIL_SENDER}>`,
-      to: emailTo,
+  const sender = { name: 'Dotpharma', email: EMAIL_SENDER };
+  client
+    .send({
+      from: sender,
+      to: [{ email: emailTo }],
       subject,
       html: template,
-      generateTextFromHTML: true,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(`Email sent: ${info.response}`);
-      }
-    });
-  } catch (error) {
-    return error;
-  }
+    })
+    .then(console.log, console.error);
 };
 
 export default sentMail;
