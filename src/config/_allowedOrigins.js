a9316f87185ddaa81322b4f16c0daa146c4324f5
benshidanny11 <benshidanny11@gmail.com>
2023-04-25 23:14:48 +0200

    Update cors

--- src/config/_allowedOrigins.js
+++ src/config/_allowedOrigins.js
@@ -6,7 +6,9 @@ dotenv.config();
 export default [
   process.env.FRONTEND_HOST,
   process.env.FRONTEND_HOST_LOCAL,
+  process.env.FRONTEND_HOST_PRODUCTION,
   'http://localhost:8000',
   'http://localhost:3000',
   'http://localhost:3001',
+
 ];
